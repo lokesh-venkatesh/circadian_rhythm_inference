e4506cc506d7a3fecbb889d48526a54c937a8f7c
lokesh-venkatesh <lokesh.venkatesh@students.iiserpune.ac.in>
2025-05-19 15:34:06 +0530

    Checkpoint: we can now safely move onto latent space analysis

--- config.py
+++ config.py
@@ -1,21 +1,36 @@
 import subprocess
 import os
 import sys
+import argparse
+
+def get_config():
+    parser = argparse.ArgumentParser(description="VAE Model Configuration")
+    parser.add_argument('--input_size', type=int, default=64*24, help='Dimensions of the input data')
+    parser.add_argument('--degree', type=int, default=3, help='Degree of fourier series for seasonal inputs')
+    parser.add_argument('--latent_size', type=int, default=4*24, help='The hours associated with each latent variable')
+    parser.add_argument('--latent_dim', type=int, default=None, help='Latent dimension')
+    parser.add_argument('--latent_filter', type=int, default=10, help='Latent filter')
+    parser.add_argument('--interim_filters', type=int, default=40, help='Interim filters')
+    parser.add_argument('--epochs', type=int, default=200, help='Number of epochs')
+    args, _ = parser.parse_known_args()
+    return args
+
+args = get_config()
 
 # define constants
-INPUT_SIZE = 64*24 # dimensions of the input data
-DEGREE = 3 # degree of fourier series for seasonal inputs
-LATENT_SIZE = 4*24 # the hours associated with each latent variable
+INPUT_SIZE = args.input_size
+DEGREE = args.degree
+LATENT_SIZE = args.latent_size
 
 # Parameters
-input_shape = INPUT_SIZE # None
-latent_dim = None # INPUT_SIZE//LATENT_SIZE # None
-latent_filter = 10
-interim_filters = 40 # 2*latent_filter
+input_shape = INPUT_SIZE
+latent_dim = args.latent_dim
+latent_filter = args.latent_filter
+interim_filters = args.interim_filters
 
 # training hyperparameters
 learning_rate = 0.001
-epochs = 100
+epochs = args.epochs
 batch_size = 32
 
 if __name__=="__main__":
@@ -26,5 +41,6 @@ if __name__=="__main__":
         # Suppress Python warnings and set environment variable to ignore warnings in subprocess
         env = os.environ.copy()
         env["PYTHONWARNINGS"] = "ignore"
-        subprocess.run([sys.executable, script], env=env)
+        # Pass the same arguments to the subprocess
+        subprocess.run([sys.executable, script] + sys.argv[1:], env=env)
         print(f"--- Finished {script} ---")
