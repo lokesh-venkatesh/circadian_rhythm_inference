1fa86f2c11ff98e6db121aed9b1c9ededa0e4584
lokesh-venkatesh <lokesh.venkatesh@students.iiserpune.ac.in>
2025-06-12 18:28:03 +0530

    Still not sure if stuff is working or not

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ def get_config():
     parser.add_argument('--latent_filter', type=int, default=10, help='Latent filter')
     parser.add_argument('--interim_filters', type=int, default=20, help='Interim filters')
     parser.add_argument('--epochs', type=int, default=200, help='Number of epochs')
-    parser.add_argument('--prior_dist_type', type=str, default='Normal', help='Prior Distribution type') # NOTE IMP!
-    parser.add_argument('--activate_phase_shift', type=bool, default=True, help='Randomly shift input phases') # NOTE IMP!
+    parser.add_argument('--prior_dist_type', type=str, default='Seasonal', help='Prior Distribution type') # NOTE IMP!
+    parser.add_argument('--activate_phase_shift', type=bool, default=False, help='Randomly shift input phases') # NOTE IMP!
     parser.add_argument('--gnrt_start', type=str, default='1970-01-01 00:00:00', help='Timestamp generation starts from')
     parser.add_argument('--gnrt_end', type=str, default='2020-12-31 16:00:00', help='Timestamp generation ends at')
     args, _ = parser.parse_known_args()
@@ -41,7 +41,7 @@ batch_size = 32
 prior_dist_type = args.prior_dist_type
 activate_phase_shift = args.activate_phase_shift
 
-os.mkdir('results', exist_ok=True)
+os.makedirs('results', exist_ok=True)
 results_directory = f'results/training_run_{prior_dist_type}_prior_{activate_phase_shift}_phase_shift_{epochs}_epochs'
 
 if __name__=="__main__":
@@ -52,10 +52,6 @@ if __name__=="__main__":
                "latent", 
                "latent_space_megaplot.py",
                "organise.py"]
-    #scripts.extend(["scripts/flatten_pca_day_of_year.py",
-    #                "scripts/flatten_pca_temp.py", 
-    #                "scripts/flatten_tsne_day_of_year.py",
-    #                "scripts/flatten_tsne_temp.py"])
     for script in scripts:
         print(f"\n--- Running {script} ---")
         # Suppress Python warnings and set environment variable to ignore warnings in subprocess
